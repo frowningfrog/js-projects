af39b00ee03c511a76d52a36c5b1735463f03d3b
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-03 10:21:42 -0700

    Initial commit

---
+++ exercise1/assignment.js
@@ -0,0 +1,21 @@
+function alertMessage(message) {
+    alert(message);
+}
+
+function getWelcomeMessage(name) {
+    console.log(name);
+}
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+// using all functions
+alertMessage('greetings, programs')
+getWelcomeMessage('Tron');
+divide(10, 2);
+multiply(4, 5);
