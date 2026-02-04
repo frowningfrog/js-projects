7fc1bda3c208c7ac811009e301afa7c6ff3eae7d
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-04 10:25:36 -0700

    add required char

--- exercise2/assignment-math.js
+++ exercise2/assignment-math.js
@@ -2,13 +2,33 @@ function areaOfCircle(radius) {
     return Math.PI * radius * radius; // Area = πr²
 }
 
+function roll(chars) {
+    return chars.charAt(Math.floor(Math.random() * chars.length)); // make password
+}
+
 function password(length) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
     let wip = '';
+
     for(let i=0; i < length; i++) {
-        wip += chars.charAt(Math.floor(Math.random() * chars.length)); // dice roll
+        wip += roll(chars);
     }
-    return wip;
+
+    let requiredChar = chars.charAt(Math.floor(Math.random() * chars.length)); // dice roll for char
+
+    for(let i=0; i < length; i++) {
+        if(wip[i] === requiredChar) {
+            break; // found the char, exit loop
+        }
+        else if(i===length-1) {
+            wip = ''; // reset password
+            for(let c=0; c < length; c++) {
+                wip += roll(chars);
+            }
+            i = -1; // restart loop
+        }
+    }
+    return [requiredChar, wip];
 }
 
 function taxCalc(price, taxRate) {
@@ -21,7 +41,11 @@ alert('The area of the circle is: ' + areaOfCircle(radius));
 
 const genPass = prompt('Enter the length for a random password:');
 
-alert('Generated password: ' + password(genPass));
+password(genPass);
+
+const [requiredChar, generatedPassword] = password(genPass);
+
+alert(`The required char: ${requiredChar} \nGenerated password: ${generatedPassword}`);
 
 const price = prompt('Enter the price of the item:');
 const taxRate = prompt('Enter the tax rate (in %):');
