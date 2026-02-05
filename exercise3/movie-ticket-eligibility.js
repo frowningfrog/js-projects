d9c99fe0fb22a6e4bee121a483106a4a5871c597
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-05 09:02:32 -0700

    add comments

--- exercise3/movie-ticket-eligibility.js
+++ exercise3/movie-ticket-eligibility.js
@@ -1,9 +1,11 @@
+// function that checks person stats
 function ageCheck(age, isStudent) {
     if (age < 18 || isStudent) {
         return true;
     }
 }
 
+// function that spits out result
 function check(person) {
     if (ageCheck(person.age, person.isStudent)) {
         console.log("Discount ticket granted ✅");
@@ -12,15 +14,16 @@ function check(person) {
     }
 }
 
+// create person objects
 let person1 = { age: 20, isStudent: true };
 let person2 = { age: 20, isStudent: false };
 let person3 = { age: 17, isStudent: true };
 let person4 = { age: 17, isStudent: false };
 
+// place people in array
 let people = [person1, person2, person3, person4];
 
+// run things for each person
 people.forEach(person => {
     check(person);
 });
-
-console.log("⬑[This line may have collapsed two console.logs to one line because they are the same message]");
