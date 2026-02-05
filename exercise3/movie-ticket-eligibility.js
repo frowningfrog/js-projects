6a3389a1181d2ff0107dbe9a5d9b9aa05417b742
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-05 09:21:53 -0700

    add person name

--- exercise3/movie-ticket-eligibility.js
+++ exercise3/movie-ticket-eligibility.js
@@ -8,17 +8,17 @@ function ageCheck(age, isStudent) {
 // function that spits out result
 function check(person) {
     if (ageCheck(person.age, person.isStudent)) {
-        console.log(`Discount ticket granted ✅`);
+        console.log(`Discount ticket granted ✅ to ${person.name}`);
     } else {
-        console.log(`Regular ticket only ❌`);
+        console.log(`Regular ticket only ❌ for ${person.name}`);
     }
 }
 
 // create person objects
-let person1 = { age: 20, isStudent: true };
-let person2 = { age: 20, isStudent: false };
-let person3 = { age: 17, isStudent: true };
-let person4 = { age: 17, isStudent: false };
+let person1 = { name: `Tron`, age: 20, isStudent: true };
+let person2 = { name: `Ram`, age: 20, isStudent: false };
+let person3 = { name: `MCP`, age: 17, isStudent: true };
+let person4 = { name: `Flynn`, age: 17, isStudent: false };
 
 // place people in array
 let people = [person1, person2, person3, person4];
