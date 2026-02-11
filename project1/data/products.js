3b08b2b274ada01bae621eed542fd09fdeca64ba
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-11 08:37:18 -0700

    update structure

---
+++ project1/data/products.js
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        id: 1,
+        name: 'hat',
+        price: 19.99
+    },
+    {
+        id: 2,
+        name: 'shirt',
+        price: 29.99
+    },
+    {
+        id: 3,
+        name: 'pants',
+        price: 39.99
+    }
+]
