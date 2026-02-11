53b56e2811253bb3d6c595753264fa6de7443066
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-11 09:31:39 -0700

    product line

--- project1/pages/products.js
+++ project1/pages/products.js
@@ -1,12 +1,12 @@
 import {products} from '../data/products.js';
 function loadProducts() {
-    document.getElementById('box').innerHTML = `
-    <p class="text-3xl font-bold mb-4">Products</p>
+  document.getElementById('box').innerHTML = `
     ${products.map(product =>
-        `<div class="product-card border p-4 mb-4">
-        <h2 class="text-xl font-semibold mb-2">${product.name}</h2>
-        <p class="text-gray-700 mb-2">$${product.price.toFixed(2)}</p>
-        </div>`).join('')}`
+      `<div class="product-card border p-4 mb-4">
+       <h2 class="text-xl font-semibold mb-2">${product.name}</h2>
+       <p class="text-gray-700 mb-2">$${product.price.toFixed(2)}</p>
+       </div>
+       </div>`).join('')}`
 }
 
 loadProducts();
