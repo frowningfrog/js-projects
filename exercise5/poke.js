8d8a72de99c9ab5342dd941448ea3cc74dd57719
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-17 10:08:48 -0700

    center poke

--- exercise5/poke.js
+++ exercise5/poke.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const masterList = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  if(!masterList.ok) {
+    console.error("Failed to fetch Pokemon data");
+    return;
+  }
   const data = await masterList.json();
 
   const picList = [];
@@ -9,24 +13,27 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   const pics = await Promise.all(picList.map(p => p.json()));
-
-  document.getElementById('box').innerHTML = `<img src="${pics[0].sprites.front_default}"><p>${data.results[0].name}</p>`;
+  let pokeName = data.results[0].name;
+  pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
+  document.getElementById('box').innerHTML = `<img src="${pics[0].sprites.front_default}"><p>1. ${pokeName}</p>`;
 
   let i = 0;
 
   document.getElementById('left').addEventListener('click', () => {
     if(i!==0){
       i--;
-      console.log(i);
-      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${data.results[i].name}</p>`;
+      pokeName = data.results[i].name;
+      pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
+      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${i+1}. ${pokeName}</p>`;
     }
   })
 
   document.getElementById('right').addEventListener('click', () => {
     if(i!==data.results.length-1){
       i++;
-      console.log(i);
-      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${data.results[i].name}</p>`;
+      pokeName = data.results[i].name;
+      pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
+      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${i+1}. ${pokeName}</p>`;
     }
   })
 
