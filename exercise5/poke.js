146cdf69119337b9b9bd3d9aef0f342b63c4a61f
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-18 09:28:04 -0700

    add details display

--- exercise5/poke.js
+++ exercise5/poke.js
@@ -15,7 +15,8 @@ document.addEventListener('DOMContentLoaded', async () => {
   const pics = await Promise.all(picList.map(p => p.json()));
   let pokeName = data.results[0].name;
   pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
-  document.getElementById('box').innerHTML = `<img src="${pics[0].sprites.front_default}"><p>1. ${pokeName}</p>`;
+  document.getElementById('box').innerHTML = `<img src="${pics[0].sprites.front_default}">`;
+  document.getElementById('pokemon-name').innerText = `1. ${pokeName}`;
 
   let i = 0;
 
@@ -24,7 +25,10 @@ document.addEventListener('DOMContentLoaded', async () => {
       i--;
       pokeName = data.results[i].name;
       pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
-      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${i+1}. ${pokeName}</p>`;
+      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}">`;
+      document.getElementById('pokemon-name').innerText = `${i+1}. ${pokeName}`;
+      document.getElementById('details').innerText = ``;
+      document.getElementById('stats').innerText = ``;
     }
   })
 
@@ -33,11 +37,24 @@ document.addEventListener('DOMContentLoaded', async () => {
       i++;
       pokeName = data.results[i].name;
       pokeName = pokeName.charAt(0).toUpperCase() + pokeName.slice(1);
-      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}"><p>${i+1}. ${pokeName}</p>`;
+      document.getElementById('box').innerHTML = `<img src="${pics[i].sprites.front_default}">`;
+      document.getElementById('pokemon-name').innerText = `${i+1}. ${pokeName}`;
+      document.getElementById('details').innerText = ``;
+      document.getElementById('stats').innerText = ``;
     }
   })
 
-  /*for(let i=0; i<data.results.length; i++) {
-    document.getElementById('list').innerHTML += `<img src="${pics[i].sprites.front_default}"><p>${data.results[i].name}</p>`;
-  };*/
+  document.getElementById('pokemon-name').addEventListener('click', async () => {
+    const deets = await fetch(`https://pokeapi.co/api/v2/pokemon/${data.results[i].name}`);
+    const details = await deets.json();
+    document.getElementById('details').innerHTML = `
+    <p>Weight: ${details.weight}</p>
+    <p>Height: ${details.height}</p>
+    <p>Type 1: ${details.types[0].type.name.charAt(0).toUpperCase() + details.types[0].type.name.slice(1)}</p>
+    <p>Type 2: ${details.types[1] ? details.types[1].type.name.charAt(0).toUpperCase() + details.types[1].type.name.slice(1) : 'none'}</p>`;
+    document.getElementById('stats').innerText = ``;
+    details.stats.forEach(stat => {
+      document.getElementById('stats').innerText += `${stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: ${stat.base_stat}\n`;
+    });
+  })
 })
