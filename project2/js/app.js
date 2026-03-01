9f36725c72c423592e33b6cf05ea7efb6f7bef71
frowningfrog <natalizedstolpinator@hotmail.com>
2026-02-28 22:32:23 -0700

    add localstorage | css

--- project2/js/app.js
+++ project2/js/app.js
@@ -1,14 +1,61 @@
-const bids = [];
-const retrieveCart = JSON.parse(localStorage.getItem('bids')) || [];
+document.addEventListener('DOMContentLoaded', () => {
+  const bids = JSON.parse(localStorage.getItem('bids')) || [];
 
-function saveBidsToLocalStorage() {
-  localStorage.setItem('bids', JSON.stringify(bids));
-}
+  function saveBidsToLocalStorage(bids) {
+    localStorage.setItem('bids', JSON.stringify(bids));
+  };
 
-document.getElementById('bid1').addEventListener('click', () => {
-  if(document.getElementById('bidder1').value && document.getElementById('bidder1').value > 0){
-    const input = document.getElementById('bidder1').value;
-    bids.push(input);
+  function updateHighestBid() {
+    if (bids.length > 0) {
+      const highestBid = bids.reduce((max, bid) => bid.amount > max.amount ? bid : max, bids[0]);
+      document.getElementById('highestBid').textContent = `${highestBid.bidder}- $${highestBid.amount}`;
+    } else {
+      document.getElementById('highestBid').textContent = 'No bids yet';
+    };
+  };
+
+  function updateBidHistory() {
+    document.getElementById('bidList').innerHTML = '';
+    for(let i=bids.length-1; i>=0; i--){
+      const bid = bids[i];
+      document.getElementById('bidList').innerHTML += `<li>${bid.bidder} - $${bid.amount}</li>`;
+    };
+  };
+
+  updateHighestBid();
+  updateBidHistory();
+
+  document.getElementById('clear').addEventListener('click', () => {
+    bids.length = 0;
+    saveBidsToLocalStorage(bids);
     console.log(bids);
-  }
-})
+    updateHighestBid();
+    updateBidHistory();
+  });
+
+  document.getElementById('bid1').addEventListener('click', () => {
+    if(document.getElementById('bidder1').value > 0){
+      const input = document.getElementById('bidder1').value;
+      bids.push({bidder: 'bidder1', amount: +input});
+    };
+    document.getElementById('bidder1').value = '';
+    document.getElementById('bidder1').placeholder = '';
+    saveBidsToLocalStorage(bids);
+    console.log(bids);
+    updateHighestBid();
+    updateBidHistory();
+  });
+
+  document.getElementById('bid2').addEventListener('click', () => {
+    if(document.getElementById('bidder2').value > 0){
+      const input = document.getElementById('bidder2').value;
+      bids.push({bidder: 'bidder2', amount: +input});
+    };
+    document.getElementById('bidder2').value = '';
+    document.getElementById('bidder2').placeholder = '';
+    saveBidsToLocalStorage(bids);
+    console.log(bids);
+    updateHighestBid();
+    updateBidHistory();
+  });
+});
