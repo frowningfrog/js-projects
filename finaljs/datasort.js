93b2172e681b0d18e187456da0971668c8cb9380
frowningfrog <natalizedstolpinator@hotmail.com>
2026-03-03 09:28:42 -0700

    dnd class roller

--- finaljs/datasort.js
+++ finaljs/datasort.js
@@ -1,10 +1,18 @@
 import { getClasses } from "./api.js";
+import { cquestions } from "./questions.js";
 
 async function fetchData() {
   const result = await getClasses();
   return result;
 }
 
-const data = await fetchData();
+function roll(num) {
+  return Math.floor(Math.random() * num);
+}
 
-console.log(data);
+const classes = await fetchData();
+
+console.log(classes);
+for(let i=0; i<1000; i++) {
+  console.log(classes[roll(classes.length)].name);
+};
