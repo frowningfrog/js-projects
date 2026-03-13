d82afbe784aeb95d7cbb85f8034f6c35d9c92362
frowningfrog <natalizedstolpinator@hotmail.com>
2026-03-13 12:38:51 -0600

    dnd answer feedback

--- finaljs/quizengine.js
+++ finaljs/quizengine.js
@@ -2,6 +2,7 @@ import { roll } from "./main.js";
 
 let score = 0;
 let current = 0;
+let correct = false;
 
 export const displayList = /*JSON.parse(localStorage.getItem('displayList')) || */[];
 
@@ -9,6 +10,7 @@ export const renderQ =  () => {
     document.getElementById('displayQ').innerText = displayList[current].q;
     let nums = [0, 1, 2, 3];
     nums.sort(() => roll(4) - 2);
+    document.getElementById('pickone').innerHTML = ``;
     ['one', 'two', 'three', 'four'].forEach((id, index) => {
         document.getElementById('pickone').innerHTML += `<button id="${id}" data-index="${index}" class="answer">${displayList[current].a[nums[index]]}</button>`;
     })
@@ -17,24 +19,33 @@ export const renderQ =  () => {
 export function setupListeners() { document.addEventListener('click', (e) => {
     if(e.target.classList.contains('answer')) {
         checkAnswer(e.target.innerText);
+    } else
+    if(e.target.classList.contains('next')) {
+        current++;
+        if(current < displayList.length) {
+            renderQ()
+        } else {
+            document.getElementById('quiz').innerHTML = ``;
+            document.getElementById('quiz').innerHTML = `
+            <div class="title">Quiz Complete!</div>
+            <div id="score">Final Score: ${score}/10</div>`;
+            localStorage.removeItem('displayList');
+        }
     }
 })};
 
 function checkAnswer(ans) {
     if(ans === displayList[current].a[0]) {
-        score++;
+        correct = true;
+    } else {
+        correct = false;
+    }
+    
+    document.getElementById('displayQ').innerHTML = correct ? 'Correct!' : 'Incorrect.';
+    document.getElementById('pickone').innerHTML = `<button class="next">next</button>`;
+    
+    if(correct) {
+        ++score;
     }
     document.getElementById('score').innerText = `Score: ${score}`;
-    
-    document.getElementById('pickone').innerHTML = '';
-    current++;
-    if(current < displayList.length) {
-        renderQ()
-    } else {
-        document.getElementById('quiz').innerHTML = ``;
-        document.getElementById('quiz').innerHTML = `
-        <div class="title">Quiz Complete!</div>
-        <div id="score">Final Score: ${score}/10</div>`;
-        localStorage.removeItem('displayList');
-    }
 };
